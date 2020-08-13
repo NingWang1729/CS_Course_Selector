10878431e75be3b863326ff8e352749fbb1ad7c4
NingWang1729 <63263298+NingWang1729@users.noreply.github.com>
2020-08-13 16:03:57 -0700

    Update CS_Class.js

--- CS_Class.js
+++ CS_Class.js
@@ -6,6 +6,21 @@ function CS_Class (name, units, professors, pre_requisites, co_requisites) {
     this.professors = professors;
     this.pre_requisites = pre_requisites;
     this.co_requisites = co_requisites;
+    
+    function has_pre_requisites(completed_classes) {
+	for (var i = 0; i <this.pre_requisites.length(); i++) {
+	    var completed = false;
+	    for (var j = 0; j < completed_classes.length(); j++) {
+		if (completed_classes[j] == this.pre_requisites[i]) {
+		    completed = true;
+		}
+	    }
+	    if (!completed) {
+		return false;
+	    }
+	}
+	return true;
+    }
 };
 
 var cs1 = new CS_Class("CS1", 1, [], [], []);
