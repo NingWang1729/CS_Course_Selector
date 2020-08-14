0b7a7f7ef078a81ce4a5af2dd0786ce36f6082b5
NingWang1729 <63263298+NingWang1729@users.noreply.github.com>
2020-08-13 18:29:34 -0700

    Add files via upload

---
+++ student.js
@@ -0,0 +1,16 @@
+class Student {
+
+    #name;
+    #completed_classes;
+    #current_classes;
+    #planned_classes;
+    
+    
+    constructor(name, completed_classes, current_classes, planned_classes) {
+	this.#name = name;
+	this.#completed_classes = completed_classes;
+	this.#current_classes = current_classes;
+	this.#planned_classes = planned_classes;
+    }
+
+}
