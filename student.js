d935b42ee800225e44aff8b0113c0b51a5f64e5b
NingWang1729 <63263298+NingWang1729@users.noreply.github.com>
2020-08-15 00:36:27 -0700

    Update student.js

--- student.js
+++ student.js
@@ -40,9 +40,68 @@ class Student {
 
     add_classes(new_classes) {
 	var ineligible = [];
-	for (int i = 0; i < new_classes.length(); i++) {
+	for (var i = 0; i < new_classes.length(); i++) {
 	    if (!add_class(new_classes[i])) {
 		ineligible.push(new_classes[i]);
+	    }
+	}
+	return ineligible;
+    }
+
+    remove_class(old_class) {
+	var index = this.#completed_classes.indexOf(old_class);
+	if (index != -1) {
+	    this.#completed_classes.splice(index, 1);
+	    return true;
+	} else {
+	    return false;
+	}
+    }
+
+    remove_classes(old_classes) {
+	var ineligible = [];
+	for (var i = 0; i < old_classes.length(); i++) {
+	    if (!remove_class(old_classes[i])) {
+		ineligible.push(old_classes[i]);
+	    }
+	}
+	return ineligible;
+    }
+
+    add_class_to_plan(new_class) {
+	if (this.#current_classes.indexOf(new_class) == -1) {
+	    this.##current_classes.push(new_class);
+	    return true;
+	} else {
+	    return false;
+	}
+    }
+
+    add_classes_to_plan(new_classes) {
+	var ineligible = [];
+	for (var i = 0; i < new_classes.length(); i++) {
+	    if (!add_class(new_classes[i])) {
+		ineligible.push(new_classes[i]);
+	    }
+	}
+	return ineligible;
+    }
+
+    remove_class_from_plan(old_class) {
+	var index = this.##current_classes.indexOf(old_class);
+	if (index != -1) {
+	    this.##current_classes.splice(index, 1);
+	    return true;
+	} else {
+	    return false;
+	}
+    }
+
+    remove_classes_from_plan(old_classes) {
+	var ineligible = [];
+	for (var i = 0; i < old_classes.length(); i++) {
+	    if (!remove_class(old_classes[i])) {
+		ineligible.push(old_classes[i]);
 	    }
 	}
 	return ineligible;
