a719757e5be29df2e6d5d0abc53d9b755e6a22fc
NingWang1729 <63263298+NingWang1729@users.noreply.github.com>
2020-08-17 17:15:20 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,27 @@
-const logger = require('./logger.js');
+const http = require('http');
+const fs = require('fs');
 
-console.log(logger);
+var handleRequest = (request, response) => {
+    response.writeHead(200, {
+	'Content-Type': 'text/html'
+    });
 
-logger.log('Hello World');
+    fs.readFile('./index.html', null, (error, data) => {
+	if (error) {
+	    response.writeHead(404);
+	    response.write('Error 404: File not found!');
+	} else {
+	    response.write(data);
+	}
+	response.end();
+    });
+}
+
+const server = http.createServer(handleRequest).listen(8000);
+
+
+server.on('connection', (socket) => {
+    console.log('New Connection...');
+});
+
+console.log('Listening on port 8000');
