25786e4dd56b302a8b52d26ffb77ca5324a73368
NingWang1729 <63263298+NingWang1729@users.noreply.github.com>
2020-08-19 22:57:33 -0700

    Update logger.js

--- template/logger.js
+++ template/logger.js
@@ -1,24 +1,3 @@
-/*
-function log(message) {
-    const EventEmitter = require('events');
-    const emitter = new EventEmitter();
-
-    emitter.on('logger', (arg) => {
-	console.log('logger', arg);
-    });
-
-    emitter.emit('logger', {data: message});
-}
-*/
-
-/*
-function log(message) {
-    //Raise an event
-    console.log(message);
-    emitter.emit('messageLogged', { id: 1, url: 'http://' });
-}
-*/
-
 const EventEmitter = require('events');
 
 var url = 'http://mylogger.io/log';
