3ccc4944cdebf62ba33eb2a886a5c30db1f79fea
Ning Wang <ningwang1729@gmail.com>
2020-08-20 00:17:27 -0700

    Updated react framework to include basic cs_class and student

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,14 @@
 import React from 'react';
 import './App.css';
+import CS_Class from './cs_class.js';
+import Student from './student.js';
 
+var cs1 = new CS_Class("CS1", 1, [], [], [], []);
 function App() {
   return (
-    <html>
-      <head>
-        <title>Hello World!</title>
-      </head>
-      <body>
-        <main className="Huh">
-          <p>Paragraph</p>
-        </main>
-        <h1>Hello There!</h1>
-      </body>
-    </html>
+    <div>
+      <h1>Hello There, {cs1.get_name()}!</h1>
+    </div>
   );
 }
 
