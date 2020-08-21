c0c741ff10e79e030e1f03a0d1a392558624e652
Ning Wang <ningwang1729@gmail.com>
2020-08-20 17:09:39 -0700

    added router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {BrowserRouter, Route, Link} from 'react-router-dom';
 import './App.css';
 import CS_Class from './cs_class.js';
 import Student from './student.js';
@@ -6,10 +7,13 @@ import Student from './student.js';
 var cs1 = new CS_Class("CS1", 1, [], [], [], []);
 function App() {
   return (
-    <div>
-      <h1>Hello There, {cs1.get_name()}!</h1>
-    </div>
+    <BrowserRouter>
+      <div>
+        <h1>Hello There, {cs1.get_name()}!</h1>
+      </div>
+    </BrowserRouter>
   );
+  
 }
 
 export default App;
