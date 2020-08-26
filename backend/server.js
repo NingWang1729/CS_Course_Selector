1a47f519e7964878cdc06eb5bb3d0195836f853e
Ning Wang <ningwang1729@gmail.com>
2020-08-26 02:12:27 -0700

    added a semi-colon

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const app = express();
 
 app.get("/cs_classes", (req, res) => {
     res.send(catalog);
-})
+});
 
 app.listen(5000, () => {
     console.log("Server started at http://localhost:5000");
